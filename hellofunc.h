a6f6db3840be1e14c4cc3311e89c3ed94021fa21
Trent Neavin <tan0004@mix.wvu.edu>
2018-09-10 16:01:34 -0400

    Adding the files for hellomake

---
+++ hellofunc.h
@@ -0,0 +1,13 @@
+//
+//  hellofunc.hpp
+//  
+//
+//  Created by Trent Neavin on 9/9/18.
+//
+
+#ifndef hellofunc_hpp
+#define hellofunc_hpp
+
+#include <stdio.h>
+
+#endif /* hellofunc_hpp */
---
+++ hellomake.h
@@ -0,0 +1,13 @@
+//
+//  hellomake.hpp
+//  
+//
+//  Created by Trent Neavin on 9/9/18.
+//
+
+#ifndef hellomake_hpp
+#define hellomake_hpp
+
+#include <stdio.h>
+
+#endif /* hellomake_hpp */
